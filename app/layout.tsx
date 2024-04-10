7c176a9d84918e9b9c272fca01c010504cfbed1c
Filip Wojda <wojdafilipdev@gmail.com>
2024-04-10 12:03:38 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import { twMerge } from 'tailwind-merge';
-import 'reactflow/dist/style.css';
-import { Toaster } from 'react-hot-toast';
 import { KeyProvider } from '@/app-context/key-context-provider';
 import { SubtopicsProvider } from '@/app-context/subtopics-context-provider';
+import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Toaster } from 'react-hot-toast';
+import 'reactflow/dist/style.css';
+import { twMerge } from 'tailwind-merge';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={twMerge(inter.className)}>
+        <Analytics />
         <Toaster />
 
         <KeyProvider>
